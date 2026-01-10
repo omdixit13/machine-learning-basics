562f1774e6d34e5ea50d2769bd5f3a8b0fadd532
om dixit <omdixit0902@gmail.com>
2026-01-10 23:47:19 +0530

    Implement linear regression model for prediction

---
+++ regression/linear_regression.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Input feature
+X = np.array([[1], [2], [3], [4], [5]])
+
+# Output label
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+prediction = model.predict([[6]])
+print("Prediction for input 6:", prediction[0])
